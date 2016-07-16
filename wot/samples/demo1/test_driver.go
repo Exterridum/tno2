7095a9b011111c2da21ac4e1083c1a27d313fd63
Robert Gallas <gallas.robert@gmail.com>
2016-07-16 22:12:43 +0200

    update test driver

--- wot/samples/demo1/test_driver.go
+++ wot/samples/demo1/test_driver.go
@@ -1,28 +1,20 @@
 package main
 
 import (
-	"github.com/conas/tno2/util/str"
 	"github.com/conas/tno2/wot"
+	"github.com/conas/tno2/wot/model"
 )
 
-type TestDriver struct {
-	actions    map[string]func(arg interface{}) interface{}
-	properties map[string]interface{}
-}
+type TestDriver struct{}
 
 func NewTestDriver() *TestDriver {
-	d := TestDriver{
-		actions:    make(map[string]func(arg interface{}) interface{}),
-		properties: make(map[string]interface{}),
-	}
-
-	d.actions["hello"] = func(arg interface{}) interface{} {
-		argMap := arg.(map[string]interface{})
-		return str.Concat("Hello ", argMap["name"].(string), "!")
-	}
-
-	return &d
+	return &TestDriver{}
 }
 
 func (d *TestDriver) Init(initParams map[string]interface{}, s *wot.Server) {
+	d.addPropsHandler(s.GetDescription())
+}
+
+func (d *TestDriver) addPropsHandler(td *model.ThingDescription) {
+
 }
