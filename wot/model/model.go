451332eb64f2eb82a22ee9376dbc49f3c16bcbae
Robert Gallas <gallas.robert@gmail.com>
2016-07-16 06:44:36 +0200

    actions sceleton

--- wot/model/model.go
+++ wot/model/model.go
@@ -11,13 +11,33 @@ type ThingDescription struct {
 	Uris       []string   `json:"uris"`
 	Encodings  []string   `json:"encodings"`
 	Properties []Property `json:"properties"`
+	Actions    []Action   `json:"actions"`
 }
 
 type Property struct {
 	Name      string    `json:"name"`
 	ValueType ValueType `json:"valueType"`
+	Unit      string    `json:"unit"`
 	Writable  bool      `json:"writable"`
 	Hrefs     []string  `json:"hrefs"`
+}
+
+type Action struct {
+	AT_Type    string     `json:"@type"`
+	Name       string     `json:"name"`
+	InputData  InputData  `json:"inputData"`
+	OutputData OutputData `json:"outputData"`
+	Hrefs      []string   `json:"hrefs"`
+}
+
+type InputData struct {
+	ValueType ValueType `json:"valueType"`
+	Unit      string    `json:"unit"`
+}
+
+type OutputData struct {
+	ValueType ValueType `json:"valueType"`
+	Unit      string    `json:"unit"`
 }
 
 type ValueType struct {
@@ -26,7 +46,7 @@ type ValueType struct {
 	Maximum int    `json:"maximum"`
 }
 
-func (td ThingDescription) ToString() string {
+func (td *ThingDescription) ToString() string {
 	out, err := json.Marshal(td)
 	if err != nil {
 		panic(err)
--- wot/samples/demo1/simple_server.go
+++ wot/samples/demo1/simple_server.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"github.com/conas/tno2/util/strings"
 	"github.com/conas/tno2/wot"
 	"github.com/conas/tno2/wot/protocol"
 )
 
-func main() {
-	server := wot.CreateFromDescriptionUri("file://../../model/testdata/case-1.json")
-	driver := NewDemoDriver()
-	server.BindSync(driver)
+var modelPrefix = "file://../../model/testdata/"
+var models = []string{"case-property-001"}
 
+func main() {
 	http := protocol.NewHttp(8080)
-	http.Bind("/sample-thing", server)
+	bindModels(http)
 	http.Start()
+}
+
+func bindModels(http *protocol.Http) {
+	for _, model := range models {
+		server := wot.CreateFromDescriptionUri(modelURI(model))
+		driver := NewDemoDriver()
+		server.BindSync(driver, make(map[string]interface{}))
+		http.Bind(strings.Concat("/", model), server)
+	}
+}
+
+func modelURI(model string) string {
+	return strings.Concat(modelPrefix, model, ".json")
 }
