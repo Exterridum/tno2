ef03f7c6a00cff2dddad679d077c3f716f3eced2
Robert Gallas <gallas.robert@gmail.com>
2016-09-19 22:00:58 +0200

    mqtt usr/pass from env

--- examples/99-public/main.go
+++ examples/99-public/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/conas/tno2/util/col"
 	"github.com/conas/tno2/wot/platform"
 )
@@ -13,8 +15,8 @@ func main() {
 		col.KV("port", 8080))
 	p.AddBackend("mqtt-1", "MQTT-1",
 		col.KV("url", "tcp://46.28.108.197:8883"),
-		col.KV("username", "mqtt1"),
-		col.KV("password", "----"))
+		col.KV("username", os.Getenv("MQTT_USER")),
+		col.KV("password", os.Getenv("MQTT_PASS")))
 	p.AddWotServer("conas-dth-esp8266-1", dhtModel, "/conas/dth-esp8266-1", "SIMPLE_URL_ENCODER", "mqtt-1", []string{"http-1"})
 	p.Start().Wait()
 }
